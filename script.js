4253992130aa56c6f2991a2a4a08eb20d1fa69db
Rithik2006-creator <poojaryrithik4@gmail.com>
2026-01-27 11:19:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-let messageBox = document.getElementById("messageBox");
-function sendMessage(){
-    let userInput = document.getElementById("userInput").value;
-    messageBox.innerHTML+=`<div class="message user" id="userMessage">${userInput}</div>`
-    getResponse(userInput)
-    document.getElementById("userInput").value=""
-}
 
-function getResponse(prompt){
-    fetch(`https://posttraumatic-preterlegal-chante.ngrok-free.dev/api/message/${prompt}`)
-    .then(data=>data.text())
-    .then(data=>messageBox.innerHTML+=`<div class="message bot"><pre>${data.replace(/\*/g, "")}</pre></div>`)
-    .catch(error=>{
-        console.log("Error"+error)
-    })
-
-}
